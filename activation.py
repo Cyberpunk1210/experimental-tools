cccac614c795d980f1c5d674f08375dfe9b57c54
Cyberpunck <Genji.1231@outlook.com>
2024-07-21 02:31:09 +0800

    add derivative

--- activation.py
+++ activation.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 x = torch.randn(10, 1)
 
 # softplus formula: f(x) = ln(1+exp(x))
+# dy = dx * torch.exp(x) / torch.exp(x) + 1
 sp = torch.log(1.0 + torch.exp(x))
 _x = F.softplus(x)
 print(f"sp and _x weather to allclose: {torch.allclose(_x, sp)}")
