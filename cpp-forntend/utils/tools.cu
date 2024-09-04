e0067c05edd38c3ca22f66f9451b7caa52894341
Cyberpunck <Genji.1231@outlook.com>
2024-09-04 22:33:16 +0800

    add cuda to cmake;

--- cpp-forntend/utils/tools.cu
+++ cpp-forntend/utils/tools.cu
@@ -1,4 +1,3 @@
-#pragma once
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <string>
@@ -6,20 +5,10 @@
 
 __device__ unsigned int block = 32;
 
-__global__ void MatAdd_kernel(at::Tensor A, at::Tensor B, at::Tensor C)
-{
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    int j = blockIdx.y * blockDim.y + threadIdx.y;
-    if (i < A.sizes()[0] && j < A.sizes()[1])
-    {
-        C[i][j] = A[i][j] + B[i][j];
-    }
-}
+// __global__ void MatAdd_kernel(at::Tensor A, at::Tensor B, at::Tensor C, int N, int M)
+// {
+//     int i = blockIdx.x * blockDim.x + threadIdx.x;
+//     int j = blockIdx.y * blockDim.y + threadIdx.y;
+//     if (i < N && j < M) C[i][j] = A[i][j] + B[i][j];
+// }
 
-
-__global__ void MatSum_kernel(at::Tensor A, int dim, bool keepdim)
-{
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-    int j = blockIdx.y * blockDim.y + threadIdx.y;
-    A[i][j]
-}
