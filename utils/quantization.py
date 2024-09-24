f0c3ef4629e8c84e96e0f97e8fb7e730971de6bb
Cyberpunck <Genji.1231@outlook.com>
2024-09-24 15:13:09 +0800

    learn smooth

--- utils/quantization.py
+++ utils/quantization.py
@@ -1,5 +1,20 @@
+import os
+import functools
+
+import tqdm
 import numpy as np
 import torch
+import torch.nn as nn
+from torch import Tensor
+from datasets import load_dataset
+from transformers import AutoTokenizer, OPTForCausalLM, Int8OPTForCausalLM
+from smoothquant import smooth_lm
+
+
+model_name = "facebook/opt-13b"
+act_scales = "act_scales/opt-13b.pt"
+dataset_path = "val.json.zst"
+output_path = "int8_models"
 
 QK8_0 = 32
 BLOCK_Q8_0 = np.dtype([('d', "<f2"), ("qs", "i1", (QK8_0, ))])
@@ -38,17 +53,97 @@ w = torch.Tensor([[-1., 0.],
 
 # 8-bit
 # find vector-wise constants: Cw & Cx
-Cx = x[:, [0, 2, 4]].abs().max(1).values
-Cw = w[[0, 2, 4], :].abs().max(0).values
-# quantize
-qxI8 = (x[:, [0, 2, 4]] * (127 / Cx)).round().to(dtype=torch.int8)
-qwI8 = (w[[0, 2, 4], :] * (127 / Cw)).round().to(dtype=torch.int8)
-# matmul
-outI32 = (qxI8 @ qwI8).to(dtype=torch.int32)
-# dequantize
-out8bF16 = outI32 * (Cx.reshape(-1, 1) * Cw.reshape(1, -1)) / 127 * 127
+"""
+scaler = 127/max(abs(w))
+w = scaler * round(w / scaler)
+"""
+def quantize_weight_per_channel_absmax(w: Tensor, n_bits: int=8):
+    wscaler = w.abs().max(-1, keepdim=True)[0]
+    q_max = 2<<(n_bits-1) - 1
+    wscaler.clamp_(min=1e-5).div_(q_max)
+    w.div_(wscaler).round_().mul_(wscaler)
+    return w
 
-# 16bit
-out16bF16 = x[:, [1, 3]] @ w[[1, 3], :]
+def quantize_activation_per_tensor_absmax(t: Tensor, n_bit: int=8):
+    t_shape = t.shape
+    t.view(-1, t_shape[1])
+    tscaler = t.abs().max()
+    q_max = 2<<(n_bit-1) - 1
+    tscaler.clamp_(min=1e-5).div_(q_max)
+    t.div_(tscaler).round_().mul_(tscaler)
+    return t
 
-out = out8bF16 + out16bF16
+
+def get_act_scales(model: torch.nn.Module, tokenizer, dataset_path, num_samples=512, seq_len=512):
+    model.eval()
+    device = next(model.parameters()).device
+    act_scales = {} # save the result of the activations statictics(max absolute)
+
+    def stat_tensor(name, tensor):
+        hidden_dim = tensor.shape[-1]
+        tensor = tensor.view(-1, hidden_dim).abs().detach()
+        comming_max = torch.max(tensor, dim=0)[0].float().cpu()
+        if name in act_scales:
+            act_scales[name] = torch.max(act_scales[name], comming_max)
+        else:
+            act_scales[name] = comming_max
+
+    def stat_input_hook(m, x, y, name):
+        if isinstance(x, tuple):
+            x = x[0]
+        stat_tensor(name, x) # statictics the result of input data
+
+    hooks = []
+    for name, m in model.name_modules():
+        if isinstance(m, nn.Linear):
+            hooks.append(
+                m.register_forward_hook(
+                    functools.partial(stat_input_hook, name=name))
+            )
+
+    # get verify data
+    dataset = load_dataset("json", data_files=dataset_path, split="train")
+    dataset = dataset.shuffle(seed=42)
+
+    # scales factor
+    for i in tqdm(range(num_samples)):
+        input_ids = tokenizer(dataset[i]["text"], return_tensors="pt",
+                              max_length=seq_len, truncation=True).input_ids.to(device)
+        model(input_ids)
+
+    for h in hooks:
+        h.remove()
+    return act_scales
+
+model = OPTForCausalLM.from_pretrained(model_name, device_map="auto", torch_dtype=torch.float16)
+act_scales = torch.load(act_scales)
+smooth_lm(model, act_scales, 0.5)
+
+@torch.no_grad()
+def smooth_ln_fcs(ln, fcs, act_scales, alpha=0.5):
+    if not isinstance(fcs, list):
+        fcs = [fcs]
+    assert isinstance(ln, nn.LayerNorm)
+    for fc in fcs:
+        assert isinstance(fc, nn.Linear)
+        assert ln.weight.numel() == fc.in_features == act_scales.numel()
+
+    device, dtype = fcs[0].weight.device, fcs[0].weight.dtype
+    act_scales = act_scales.to(device=device, dtype=dtype)
+    # 计算权重的最大绝对值
+    weight_scales = torch.cat([fc.weight.abs().max(
+        dim=0, keepdim=True)[0] for fc in fcs], dim=0)
+    weight_scales = weight_scales.max(dim=0)[0].clamp(min=1e-5)
+    # 按照公式(4)计算S
+    scales = (act_scales.pow(alpha) / weight_scales.pow(1-alpha)
+              ).clamp(min=1e-5).to(device).to(dtype)
+    # 把激活X的scales提前放到前一层的layer_norm的权重中去
+    ln.weight.div_(scales)
+    ln.bias.div_(scales)
+    # 权重直接乘以S
+    for fc in fcs:
+        fc.weight.mul_(scales.view(1, -1))
+
+
+
+
